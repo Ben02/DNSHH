42af6369e74e6c7c499d7d1896df622f1aff33ef
m208 <1010336298@qq.com>
2013-05-12 15:45:58 +0800

    Add tag and category page.

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ else
 	  }
   get_header();
 ?>
+<?php if(is_category()) { ?>
+<div class="alert alert-info"><h4><?php echo single_cat_title(); ?></h4></div>
+<?php } elseif (is_tag()) { ?>
+<div class="alert alert-info"><h4><?php echo single_tag_title('', true); ?></h4></div>
+<?php } ?>
  <div id="content">
  <?php include(TEMPLATEPATH . '/index_list.php'); ?>
 </div>
